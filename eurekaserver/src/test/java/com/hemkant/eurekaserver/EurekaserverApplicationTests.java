c21207217dfcfd27039896f08baea0f5d345dfc1
Hem Kant <hemkant.india@gmail.com>
2022-09-24 14:00:17 +0530

    Eureka Service Discovery for Mircroservices
    Eureka Service Discovery for Mircroservices

---
+++ eurekaserver/src/test/java/com/hemkant/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemkant.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
